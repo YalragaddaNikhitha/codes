c8c4d5293276253190cd64b4dded1912363cd0de
YalragaddaNikhitha <44520005+YalragaddaNikhitha@users.noreply.github.com>
2018-10-27 13:18:42 +0530

    Add files via upload

---
+++ pdo.php
@@ -0,0 +1,9 @@
+<?php
+echo "Y NIKHITHA\n";
+$pdo = new PDO('mysql:host=localhost;port=8889;dbname=misc', 
+   'fred', 'zap');
+// See the "errors" folder for details...
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+
+
